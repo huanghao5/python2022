50213236e74af6f54f9394b7a0e6265695a94caf
huanghao <hh@anhereef.com>
2022-04-13 16:27:05 +0800

    函数中参数详解

--- base/函数.py
+++ base/函数.py
@@ -6,5 +6,20 @@ def func(name):
     print("name {0}".format(name))
 
 
+def func1(age=18):
+    print("age {0}".format(age))
+
+
+def func2(*args, **kwargs):
+
+    print("*args {0}, **kwargs {1}".format(args,kwargs))
+    print(type(args))
+
+
+
 if __name__ == '__main__':
     func(name="张三")
+
+    func1(age=19)
+
+    func2((1,2),name="lisi")
